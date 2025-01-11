0136cbf4c3ae1a554be5b42cf9357c7ab5f67230
CristovaoAlexander <alex.cafr.fac@gmail.com>
2025-01-11 16:05:36 -0300

    incluir arquivo CreateTable

---
+++ CreateTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE WebCrawlerExecutionLog (
+    ExecutionId INT IDENTITY(1,1) PRIMARY KEY,
+    StartTime DATETIME,
+    EndTime DATETIME,
+    PageCount INT,
+    LinhasCount INT,
+    JsonFilePath NVARCHAR(255)
+);
+
+select * from WebCrawlerExecutionLog;
